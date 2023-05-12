15ebeecb8d2f7186729e0b6a384c74c194ccb992
AmandaRazmus <118416814+AmandaRazmus@users.noreply.github.com>
2023-05-11 17:44:04 -0700

    Seeding the db and adding productRoutes

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin user', 
+    email: 'info@gmail.com',
+    password: '123456', 
+    isAdmin: true
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  }
+]
+
+export default users
+
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,7 +4,10 @@ const reviewSchema = mongoose.Schema({
     name: {type: String, required:true},
     rating: {type: Number, required: true}, 
     comment: {type: String, required: true}
+}, {
+    timestamps: true
 })
+
 const productSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
