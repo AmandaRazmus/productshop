9a268d44cb4e672e835de06a2a70ed6a8bb81b28
AmandaRazmus <118416814+AmandaRazmus@users.noreply.github.com>
2023-05-15 16:25:40 -0700

    Authenticate user

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,15 @@
 import express from 'express'; 
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js'; 
-
+import { getProductById, getProducts } from '../controllers/productController.js'
 
 const router = express.Router()
 
-//this is going to the MongoDB and getting the products from the database through the Product model
-router.get('/', asyncHandler(async(req, res) => {
-   const products = await Product.find({})
-   res.json(products)
-}))
+// @desc    Fetch all products
+// @route   GET /api/products/
+// @access  public
+router.get('/', getProducts)
 
-//this is going to the MongoDB and getting the products from the database using the id
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product){
-    res.json(product)
-    }else{
-        res.status(404).json({message: 'Product not found'})
-    }
-}))
+router.get('/:id', getProductById)
 
 export default router
