c12a5fa86d47057e2d2c1aa1361511d8e4f1871a
amandarazmus <amandarazmus@gmail.com>
2023-11-07 09:19:39 -0800

    update .json

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import connectDB from './config/db.js';
 import errorHandler from './middleware/errorMiddleware.js';
 
-const favicon = import('serve-favicon')
 const app = express();
 dotenv.config()
 connectDB()
@@ -28,9 +27,6 @@ if (process.env.NODE_ENV === 'production'){
   })
 }
 
-
-
 app.use(errorHandler)
- 
 const PORT = process.env.PORT
-app.listen(8080, console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, console.log(`Server is running on port ${PORT}`))
