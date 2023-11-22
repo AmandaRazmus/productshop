9b21a47ff67109eb21e9a55098364ff4f07e349e
amandarazmus <amandarazmus@gmail.com>
2023-11-21 17:24:14 -0800

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,16 +7,13 @@ const connectDB = async () => {
   try{
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true
-    })
-
+      useNewUrlParser: true
+    });
     console.log(`MongoDb Connected: ${conn.connection.host}`)
   } catch(error){
     console.error(`Error: ${error.message}`)
-    process.exit(1)
+    process.exit(1);
   }
-
 }
 
 
--- backend/server.js
+++ backend/server.js
@@ -31,5 +31,10 @@ if (process.env.NODE_ENV === 'production'){
 }
 
 app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port {PORT}`));
+
+/*
 const PORT = process.env.PORT
-app.listen(PORT, console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, console.log(`Server is running on port ${PORT}`))*/
